ad63c856f8610d1bb2517cde0089a1f9b43bc9bb
Jonathan McComb <jmccomb@infoasis.com.au>
2020-03-27 13:23:21 +1100

    UI touchup to single station

--- ui/single_station.R
+++ ui/single_station.R
@@ -4,15 +4,18 @@ box(title = "Process Single Station", status = "primary", width = 12,
         tabPanel(title = "1. Load Dataset",
             id = "process_single_station_step_1",
             value = "process_single_station_step_1",
-            wellPanel(title = "Dataset",
+            wellPanel(
+                h4("Load station data"),
                 fileInput('dataFile', NULL, accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-                uiOutput("loadDatasetText")),
-            wellPanel(title = "Metadata",
-                    textInput("stationName", "Station name (used in output file names):"),
-                    numericInput("stationLat", "Latitude:", 0, min = -90, max = 90),
-                    numericInput("stationLon", "Longitude:", 0, min = -180, max = 180),
-                    numericInput("startYear", "Base Period Start year:", 1971, min = 0),
-                    numericInput("endYear", "Base Period End year:", 2000, min = 0)
+                uiOutput("loadDatasetText")
+            ),
+            wellPanel(
+                h4("Provide metadata"),
+                textInput("stationName", "Station name (used in output file names):"),
+                numericInput("stationLat", "Latitude:", 0, min = -90, max = 90),
+                numericInput("stationLon", "Longitude:", 0, min = -180, max = 180),
+                numericInput("startYear", "Base Period Start year:", 1971, min = 0),
+                numericInput("endYear", "Base Period End year:", 2000, min = 0)
             ),
             br(),
             actionButton("btn_next_process_single_station_step_1", label = "Next >")
@@ -23,13 +26,13 @@ box(title = "Process Single Station", status = "primary", width = 12,
                 htmlOutput("dataFileLoadedWarning"),
                 conditionalPanel(
                     condition = 'output.dataFileLoaded',
-                    wellPanel(title = "Quality Control",
+                    wellPanel(
                         actionButton("doQualityControl", "Check Quality"),
                         textOutput("qualityControlError")
                     ),
                     conditionalPanel(
                         condition = "output.qualityControlError == ''",
-                        wellPanel(title = "Evaluate Quality Control output",
+                        wellPanel(h4("Evaluate Quality Control output"),
                             uiOutput("qcLink")
                         )
                     )
@@ -40,10 +43,81 @@ box(title = "Process Single Station", status = "primary", width = 12,
         tabPanel(title = "3. Calculate Climate Indices",
             id = "process_single_station_step_3",
             value = "process_single_station_step_3",
-            wellPanel(title = "Calculate",
+            wellPanel(
                 conditionalPanel(
                     condition = "output.qualityControlError != ''",
                     wellPanel("Please check data quality.")
+                ),
+                conditionalPanel(
+                    condition = "output.qualityControlError == ''",
+                    wellPanel(
+                        h4("Provide User Parameters"),
+                        fluidRow(
+                            column(6,
+                                textInput("plotTitle", "Plot title:")
+                            )
+                        ),
+                        fluidRow(
+                            column(12, uiOutput("loadParamHelpText"))
+                        ),
+                        wellPanel(
+                            fluidRow(
+                                column(4,
+                                    numericInput("wsdin", "d for WSDId Days (1 =< d <= 10):", 1, min = 1, max = 10),
+                                    numericInput("csdin", "d for CSDId Days (1 =< d <= 10):", 1, min = 1, max = 10),
+                                    numericInput("rxnday", "d for Rxdday Days (d >= 1):", 3, min = 1),
+                                    numericInput("txtn", "d for TXdTNd and TXbdTNbd (d >= 1):", 2, min = 1)
+                                ),
+                                column(4,
+                                    numericInput("hdd", "Base temperature for HDDHeat:", 18),
+                                    numericInput("cdd", "Base temperature for CDDHeat:", 18),
+                                    numericInput("gdd", "Base temperature for GDDgrow:", 10),
+                                    numericInput("rnnmm", "Number of days precip >= nn (Rnnmm; nn >= 0):", 30, min = 0),
+                                    numericInput("spei", "SPEI/SPI custom monthly time scale (must be a positive number):", 24, min = 1)
+                                ),
+                                column(4,
+                                    wellPanel(
+                                        h4("Specify custom thresholds"),
+                                        strong("Custom index counting days above or below a given threshold (e.g. number of days where TX > 40, named TXgt40)"),
+                                        br(),
+                                        selectInput("custVariable", label = "Variable:",
+                                            choices = list("TN", "TX", "TM", "PR", "DTR"),
+                                            selected = "TN"
+                                        ),
+                                        selectInput("custOperation", label = "Operation:",
+                                            choices = list(">", ">=", "<", "<="),
+                                            selected = ">"
+                                        ),
+                                        numericInput("custThreshold", "Threshold:", 0)
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    wellPanel(
+                        fluidRow(
+                            column(6,
+                                conditionalPanel(
+                                    condition = "output.qualityControlError == ''",
+                                    actionButton("calculateIndices", "Calculate Indices"),
+                                    textOutput("indiceCalculationError")
+                                ),
+                                conditionalPanel(
+                                    condition = "output.qualityControlError != ''",
+                                    wellPanel("Please load data and perform Quality Control check.")
+                                )
+                            )
+                        ),
+                        fluidRow(
+                            column(6,
+                                conditionalPanel(
+                                    condition = "output.indiceCalculationError == ''",
+                                    h4("View Indices"),
+                                    uiOutput("indicesLink")
+                                )
+                            )
+                        )
+                    )
                 )
             ),
             br(),
@@ -54,27 +128,24 @@ box(title = "Process Single Station", status = "primary", width = 12,
             value = "process_single_station_step_4",
             fluidRow(
                 column(6,
-                    h4('9. Load Sector Data'),
+                    h4('Load sector data'),
                     wellPanel(
                         fileInput('sectorDataFile', NULL, accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
                         uiOutput("loadSectorDataText")
                     ),
-                    h4('10. Sector Data Infomation'),
+                    h4('Specify chart title'),
                     wellPanel(
-                        textInput("sectorPlotName", "Plot name:"),
+                        textInput("sectorPlotName", "Chart title:"),
                         checkboxInput("detrendCheck", "Detrend data", value = TRUE, width = NULL)
                     )
                 )
             ),
             fluidRow(
                 column(6,
-                    h4('11. Make correlation plots'),
+                    h4('Make correlation plots'),
                     conditionalPanel(
                         condition = "output.indiceCalculationError != ''",
-                        wellPanel(
-                            "Please complete step 7: ",
-                            tags$b("Calculate Indices.")
-                        )
+                        wellPanel("Please complete previous step - 3. Calculate Climate Indices.")
                     ),
                     conditionalPanel(
                         condition = "output.indiceCalculationError == ''",
@@ -83,21 +154,21 @@ box(title = "Process Single Station", status = "primary", width = 12,
                             textOutput("sectorCorrelationError")
                         )
                     ),
-                    h4('12. View correlation'),
+                    h4('View correlation'),
                     conditionalPanel(
                         condition = "output.sectorCorrelationError== ''",
                         wellPanel(
-                        uiOutput("sectorCorrelationLink")
+                            uiOutput("sectorCorrelationLink")
                         )
                     ),
                     conditionalPanel(
                         condition = "output.sectorCorrelationError != ''",
                         wellPanel(
-                        "Please complete step 11: ", tags$b("Calculate Correlation.")
+                            "Click Calculate correlations button above. Correlation plots available here after calculations completed."
                         )
                     )
                 )
-            ) 
-        )
+            )
+        ) 
     )
 )
