6cd189ad9eaf3075a68057695fbf1af176e4e606
Jonathan McComb <jmccomb@infoasis.com.au>
2020-04-03 10:39:11 +1100

    Update landing_page.R

--- ui/landing_page.R
+++ ui/landing_page.R
@@ -1,14 +1,12 @@
 tabPanel(title="ClimPACT",value="frontPage",
-  fluidPage(
-    div(class = "jumbotron",
-      div(class="container",
-        h1(class="display-4", "ClimPACT"),
-        p(class="lead", "An R software package that calculates ET-SCI indices.")
-      )
-    ),
-    div(class="landing-content",
-      includeMarkdown(file.path("ui", "getting_started.md"))
-    ) 
-    , div(class="photo-credit", "Background photo by Dominik Schröder on Unsplash")
-  )
+  div(class = "jumbotron",
+    div(class="container",
+      h1(class="display-4", "ClimPACT"),
+      p(class="lead", "An R software package that calculates ET-SCI indices.")
+    )
+  ),
+  div(class="landing-content",
+    includeMarkdown(file.path("ui", "getting_started.md"))
+  ) 
+  , div(class="photo-credit", "Background photo by Dominik Schröder on Unsplash")
 )
