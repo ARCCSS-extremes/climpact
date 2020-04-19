3663bbc496f964fc1035189633a2b8b9af00e24d
Jonathan McComb <jmccomb@infoasis.com.au>
2020-04-20 09:55:11 +1000

    Changed global vars in griddedStep1.R to reactiveVal vars

--- modules/griddedStep1.R
+++ modules/griddedStep1.R
@@ -11,7 +11,7 @@ griddedStep1 <- function(input, output, session, climpactUI) {
           "You will see \"Done\" printed below the \"Calculate NetCDF indices\" button when ClimPACT is finished."),
         br(),
         br(),
-        paste0("Your output will be created in ", gridOutDir),
+        paste0("Your output will be created in ", gridOutDir()),
         footer = tagList(
           modalButton("Cancel"),
           actionButton(ns("proceedGridded"), "Calculate Indices")
@@ -39,32 +39,33 @@ griddedStep1 <- function(input, output, session, climpactUI) {
     )
   }
 
-  gridNcFiles  <<- NULL
-  gridNcThresh <<- NULL
-  gridOutDir   <<- NULL
+  gridNcFiles  <- reactiveVal(NULL)
+  gridNcThresh <- reactiveVal(NULL)
+  gridOutDir   <- reactiveVal(NULL)
+
   observeEvent(input$selectNcFiles, {
-    gridNcFiles <<- fchoose(filters=ncFilter)
+    gridNcFiles(fchoose(filters=ncFilter))
     output$ncFilePrint <- renderText({
-      print(paste0("Input file(s): ", paste0(gridNcFiles, collapse = ", ")))
+      print(paste0("Input file(s): ", paste0(gridNcFiles(), collapse = ", ")))
     })
   })
 
   observeEvent(input$selectOutDir, {
-    gridOutDir <<- dchoose()
+    gridOutDir(dchoose())
     output$outDirPrint <- renderText({
-      print(paste0("Output directory: ", gridOutDir))
+      print(paste0("Output directory: ", gridOutDir()))
     })
   })
 
   observeEvent(input$selectNcFilesThreshInput, {
-    gridNcThresh <<- fchoose(filters=ncFilter)
+    gridNcThresh(fchoose(filters=ncFilter))
     output$ncFilePrintThreshInput <- renderText({
-      paste0("Threshold file: ", paste0(gridNcThresh, collapse=", "))
+      paste0("Threshold file: ", paste0(gridNcThresh(), collapse=", "))
     })
   })
 
   # Global vars required as class level vars not updated from within methods.
-  user_wrapper_file <<- NULL
+  user_wrapper_file <- reactiveVal("")
 
   # Create the ncdf wrapper file that will be executed from the Linux command line.
   # All code needs to be placed inside the renderText() function so that validate() can abort this code if any errors are detected.
@@ -73,11 +74,11 @@ griddedStep1 <- function(input, output, session, climpactUI) {
       # Validate inputs
       # ------------------------------------------------------------------ #
       validate(
-        need(!is.null(gridNcFiles), message = "Please specify input file(s)."),
+        need(!is.null(gridNcFiles()), message = "Please specify input file(s)."),
         need(input$prName, message = "Please specify the name of the precipitation variable as it is recorded in its netCDF file"),
         need(input$tnName, message = "Please specify the name of the minimum temperature variable as it is recorded in its netCDF file."),
         need(input$txName, message = "Please specify the name of the maximum temperature variable as it is recorded in its netCDF file."),
-        need(!is.null(gridOutDir), message = "Please specify output directory."),
+        need(!is.null(gridOutDir()), message = "Please specify output directory."),
         need(input$fileConvention, message = "Please specify an output filename convention."),
         need(input$instituteID, input$instituteName, message = "Please specify institute name and ID."),
         need(input$baseBegin, input$BaseEnd, message = "Please specify start and end year of base period (e.g. 1990)"),
@@ -89,14 +90,14 @@ griddedStep1 <- function(input, output, session, climpactUI) {
       # Make and edit wrapper file with user preferences
       # ------------------------------------------------------------------ #
       master.ncdf.gridded.wrapper.file <- "climpact.ncdf.wrapper.r"
-      user_wrapper_file <<- paste0("climpact.ncdf.", input$instituteID, ".wrapper.r")
-      file.copy(master.ncdf.gridded.wrapper.file, user_wrapper_file, overwrite = TRUE)
+      user_wrapper_file(paste0("climpact.ncdf.", input$instituteID, ".wrapper.r"))
+      file.copy(master.ncdf.gridded.wrapper.file, user_wrapper_file(), overwrite = TRUE)
 
-      wraptext <- readLines(user_wrapper_file)
+      wraptext <- readLines(user_wrapper_file())
       fileText <- ""
-      for (i in 1:length(gridNcFiles)) {
-        fileText <- paste0(fileText, "\"", gridNcFiles[i], "\"")
-        if (i < length(gridNcFiles)) {
+      for (i in 1:length(gridNcFiles())) {
+        fileText <- paste0(fileText, "\"", gridNcFiles()[i], "\"")
+        if (i < length(gridNcFiles())) {
           fileText <- paste0(fileText, ",")
         }
       }
@@ -104,7 +105,7 @@ griddedStep1 <- function(input, output, session, climpactUI) {
       # Take care of backward slashes (in Windows)
       fileText <- gsub(pattern="\\\\", replace="/", x = fileText)
 
-      tmp1 <- gridOutDir
+      tmp1 <- gridOutDir()
       split_path <- function(x) {
         if (dirname(x) == x) {
           x
@@ -129,13 +130,13 @@ griddedStep1 <- function(input, output, session, climpactUI) {
       if (input$nCores > 1) {
         wraptext <- gsub(pattern = ".*cores=FALSE.*", replace = paste0("cores=", input$nCores), x = wraptext)
       }
-      if (exists("gridNcThresh") && !is.null(gridNcThresh)) {
-        wraptext <- gsub(pattern = ".*thresholds.files=NULL.*", replace = paste0("thresholds.files=\"", gridNcThresh, "\""), x = wraptext)
+      if (!is.null(gridNcThresh())) {
+        wraptext <- gsub(pattern = ".*thresholds.files=NULL.*", replace = paste0("thresholds.files=\"", gridNcThresh(), "\""), x = wraptext)
       }
       wraptext <- gsub(pattern = ".*EHF_DEF = .*", replace = paste0("EHF_DEF=\"", input$EHFcalc, "\""), x = wraptext)
       wraptext <- gsub(pattern = ".*maxvals=10.*", replace = paste0("maxvals=", input$maxVals), x = wraptext)
 
-      writeLines(wraptext, con = user_wrapper_file)
+      writeLines(wraptext, con = user_wrapper_file())
 
       showModal(infoDialog())
 
@@ -148,7 +149,7 @@ griddedStep1 <- function(input, output, session, climpactUI) {
     disable("calculateGriddedIndices")
     removeModal()
 browser()
-    out <- tryCatch(system(paste0("Rscript ", user_wrapper_file), intern = TRUE),
+    out <- tryCatch(system(paste0("Rscript ", user_wrapper_file()), intern = TRUE),
               error = function(cond) {
                 error <- TRUE
                 showModal(errorDialog(cond$message))
@@ -159,19 +160,19 @@ browser()
               #   return(paste0("Warning: ", cond$message))
               # },
               finally = {
-                if (file.exists(user_wrapper_file)) {
-                  invisible(file.remove(user_wrapper_file))
+                if (file.exists(user_wrapper_file())) {
+                  invisible(file.remove(user_wrapper_file()))
                 }
               })
     output$ncGriddedDone <- renderText({
       if (is.character(data) && !error) {
-        HTML("Done.", paste0("Look in the following directory for your output: ", gridOutDir), "<br />", out)
+        HTML("Done.", paste0("Look in the following directory for your output: ", gridOutDir()), "<br />", out)
       } else {
         HTML(out)
       }
     })
 
-    gridNcThresh <<- NULL
+    gridNcThresh(NULL)
     enable("calculateGriddedIndices")
   })
 
