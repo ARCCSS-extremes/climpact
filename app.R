cdd2520142f1283c499b762397e014d7e91b5546
Jonathan McComb <jmccomb@infoasis.com.au>
2020-04-20 09:54:33 +1000

    Rename files and folders

--- app.R
+++ app.R
@@ -13,7 +13,7 @@
 #
 # TECHNICAL NOTES
 #   - See server/ClimPACT_developer_documentation.md for a technical document describing the major parts of this code. Refer
-# 	  to this for insight when making changes such as adding indices.
+#     to this for insight when making changes such as adding indices.
 #
 # HISTORY
 #   The ClimPACT code has evolved very heavily since it's original incarnation in the form of the single source file climpact.r.
@@ -40,32 +40,10 @@
 #   Hongang to check Sandra's code and add new indices - from 2012-11-05
 #   modified 2013, James Goldie - overhaul of code
 #   overhaul 2015-2017, Nicholas Herold and Nicholas Hannah (PCIC R package implementation, R Shiny interface, gridded indices, new indices).
+#   modified 2020 by Jonathan McComb  - Shiny code organised into modules.
 
-# Source files and load libraries.
+# Load libraries
 # Note, other libraries are sourced at different points in the program depending on what functions the user interacts with.
-source("server/climpact.GUI-functions.r")
-source("server/sector_correlation.R")
-source("server/climpact.etsci-functions.r")
-source("custom_errors.r")
-source("models/climdexInputParams.R")
-source("models/sectorInputParams.R")
-source("models/weatherObservationsRequest.R")
-source("models/weatherStation.R")
-source("modules/singleStationStep1.R")
-source("modules/singleStationStep1UI.R")
-source("modules/singleStationStep2.R")
-source("modules/singleStationStep2UI.R")
-source("modules/singleStationStep3.R")
-source("modules/singleStationStep3UI.R")
-source("modules/singleStationStep4.R")
-source("modules/singleStationStep4UI.R")
-source("modules/batchStep1.R")
-source("modules/batchStep1UI.R")
-source("modules/griddedStep1.R")
-source("modules/griddedStep1UI.R")
-source("modules/griddedStep2.R")
-source("modules/griddedStep2UI.R")
-source("services/zipper.R")
 package.check()
 library(zoo)
 library(zyp)
@@ -108,11 +86,34 @@ if (isLocal) {
   # Increase file upload limit to something extreme to account for large files.
   options(shiny.maxRequestSize = 1000000 * 1024 ^ 2)
   ncFilter <<- matrix(c("NetCDF", "*.nc"), 1, 2, byrow = TRUE)
-  gridNcFiles <<- gridOutDir <<- gridNcFilesThresh <<- gridOutDirThresh <<- NULL
 }
 
-source("ui_support.R")
-source("ui/ui.R")
+# Source files
+source("server/climpact.GUI-functions.r")
+source("server/sector_correlation.R")
+source("server/climpact.etsci-functions.r")
+source("models/custom_errors.r")
+source("models/climdexInputParams.R")
+source("models/sectorInputParams.R")
+source("models/weatherObservationsRequest.R")
+source("models/weatherStation.R")
+source("modules/singleStationStep1.R")
+source("modules/singleStationStep1UI.R")
+source("modules/singleStationStep2.R")
+source("modules/singleStationStep2UI.R")
+source("modules/singleStationStep3.R")
+source("modules/singleStationStep3UI.R")
+source("modules/singleStationStep4.R")
+source("modules/singleStationStep4UI.R")
+source("modules/batchStep1.R")
+source("modules/batchStep1UI.R")
+source("modules/griddedStep1.R")
+source("modules/griddedStep1UI.R")
+source("modules/griddedStep2.R")
+source("modules/griddedStep2UI.R")
 source("server/server.R")
+source("services/zipFiles.R")
+source("ui/uiSupport.R")
+source("ui/ui.R")
 
 shinyApp(ui, server)
