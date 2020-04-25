8e2b206c21ee1703615965de7db1f44ef48784bc
Jonathan McComb <jmccomb@infoasis.com.au>
2020-04-25 12:07:49 +1000

    Updated tab titles

--- ui/single_station.R
+++ ui/single_station.R
@@ -1,21 +1,21 @@
 box(title = "Process Single Station", status = "primary", width = 12, solidHeader = TRUE,
   tabBox(id = "process_single_station", width = 12,
-    tabPanel(title = "1. Load",
+    tabPanel(title = tagList(HTML("1. Load&nbsp;&nbsp;&nbsp;&nbsp;")),
         id = "process_single_station_step_1",
         value = "process_single_station_step_1",
         singleStationStep1UI("ui")
     ),
-    tabPanel(title = "2. Check",
+    tabPanel(title = tagList(icon("chevron-circle-right"), HTML("&nbsp;&nbsp;&nbsp;"), HTML("2. Check&nbsp;&nbsp;&nbsp;&nbsp;")),
         id = "process_single_station_step_2",
         value = "process_single_station_step_2",
         singleStationStep2UI("ui")
     ),
-    tabPanel(title = "3. Calculate",
+    tabPanel(title = tagList(icon("chevron-circle-right"), HTML("&nbsp;&nbsp;&nbsp;"), HTML("3. Calculate&nbsp;&nbsp;&nbsp;&nbsp;")),
         id = "process_single_station_step_3",
         value = "process_single_station_step_3",
         singleStationStep3UI("ui")
     ),
-    tabPanel(title = "4. Correlate",
+    tabPanel(title = tagList(icon("chevron-circle-right"), HTML("&nbsp;&nbsp;&nbsp;"), HTML("4. Correlate&nbsp;&nbsp;&nbsp;&nbsp;")),
         id = "process_single_station_step_4",
         value = "process_single_station_step_4",
         singleStationStep4UI("ui")
