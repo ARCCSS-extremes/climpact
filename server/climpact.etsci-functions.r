5ad9ecebeb9de5468ef8c738ae17c5236e38a85a
heroldn <heroldn@mailbox.org>
2020-10-29 08:51:48 +1100

    test a temporary fix for ostensible bug in climdex.pcic. zeroes were being returned for inbase years with data when minimum threshold for inbase data was not being met

--- server/climpact.etsci-functions.r
+++ server/climpact.etsci-functions.r
@@ -170,6 +170,68 @@ climdex.rnnmm <- function(ci, threshold=1,freq=c("monthly", "annual")) {
 # Value of 95th percentile of TX
 climdex.tx95t <- function(ci, freq=c("monthly", "annual")) { stopifnot(!is.null(ci@data$tmax),!is.null(ci@quantiles$tmax)); return(ci@quantiles$tmax$outbase$q95) }
 
+
+
+
+
+################# TEMPORARY INCLUSION FROM CLIMDEX.PCIC
+# The following functions have been copied from climdex.pcic while a bug in percent.days.op.threshold is present. 
+# Once/if this is fixed in climdex.pcic then the following lines can be deleted. Issue has been logged on their github page.
+# nherold, Nov 2020.
+get.years <- function(dates) {
+  return(as.POSIXlt(dates)$year + 1900)
+}
+
+percent.days.op.threshold <- function(temp, dates, jdays, date.factor, threshold.outside.base, base.thresholds, base.range, op='<', max.missing.days) {
+  f <- match.fun(op)
+  dat <- f(temp, threshold.outside.base[jdays])
+  
+  inset <- dates >= base.range[1] & dates <= base.range[2]
+  ## Don't use in-base thresholds with data shorter than two years; no years to replace with.
+  if(sum(inset) > 0 && length(dates) >= 360 * 2) {
+    jdays.base <- jdays[inset]
+    years.base <- get.years(dates[inset])
+
+    ## Get number of base years, subset temp data to base period only.
+    temp.base <- temp[inset]
+    years.base.range <- range(years.base)
+    byrs <- (years.base.range[2] - years.base.range[1] + 1)
+
+    ## Linearize thresholds, then compare them to the temperatures
+    bdim <- dim(base.thresholds)
+    dim(base.thresholds) <- c(bdim[1] * bdim[2], bdim[3])
+    yday.byr.indices <- jdays.base + (years.base - get.years(base.range)[1]) * bdim[1]
+    f.result <- f(rep(temp.base, byrs - 1), base.thresholds[yday.byr.indices,])
+    dim(f.result) <- c(length(yday.byr.indices), bdim[3])
+
+    ## Chop up data along the 2nd dim into a list; sum elements of the list
+#    dat[inset] <- rowSums(f.result, na.rm=TRUE) / (byrs - 1)
+    # Replace above line to properly return NA when all values are NA (problem was occurring because sum(c(Na,NA)) = 0, when it should equal NA).
+    dat[inset] <- apply(f.result,1,function(x) if (all(is.na(x))) x[NA_integer_] else sum(x, na.rm = TRUE) / (byrs - 1))
+  }
+  dat[is.nan(dat)] <- NA
+  if(missing(date.factor))
+    return(dat)
+  na.mask <- get.na.mask(dat, date.factor, max.missing.days)
+  ## FIXME: Need to monthly-ize the NA mask calculation, which will be ugly.
+  ret <- tapply.fast(dat, date.factor, mean, na.rm=TRUE) * 100 * na.mask
+  ret[is.nan(ret)] <- NA
+  return(ret)
+}
+
+climdex.tn10p <- function(ci, freq=c("monthly", "annual")) { stopifnot(!is.null(ci@data$tmin) && !is.null(ci@quantiles$tmin)); return(percent.days.op.threshold(ci@data$tmin, ci@dates, ci@jdays, ci@date.factors[[match.arg(freq)]], ci@quantiles$tmin$outbase$q10, ci@quantiles$tmin$inbase$q10, ci@base.range, "<", ci@max.missing.days[match.arg(freq)]) * ci@namasks[[match.arg(freq)]]$tmin) }
+
+climdex.tx10p <- function(ci, freq=c("monthly", "annual")) { stopifnot(!is.null(ci@data$tmax) && !is.null(ci@quantiles$tmax)); return(percent.days.op.threshold(ci@data$tmax, ci@dates, ci@jdays, ci@date.factors[[match.arg(freq)]], ci@quantiles$tmax$outbase$q10, ci@quantiles$tmax$inbase$q10, ci@base.range, "<", ci@max.missing.days[match.arg(freq)]) * ci@namasks[[match.arg(freq)]]$tmax) }
+
+climdex.tn90p <- function(ci, freq=c("monthly", "annual")) { stopifnot(!is.null(ci@data$tmin) && !is.null(ci@quantiles$tmin)); return(percent.days.op.threshold(ci@data$tmin, ci@dates, ci@jdays, ci@date.factors[[match.arg(freq)]], ci@quantiles$tmin$outbase$q90, ci@quantiles$tmin$inbase$q90, ci@base.range, ">", ci@max.missing.days[match.arg(freq)]) * ci@namasks[[match.arg(freq)]]$tmin) }
+
+climdex.tx90p <- function(ci, freq=c("monthly", "annual")) { stopifnot(!is.null(ci@data$tmax) && !is.null(ci@quantiles$tmax)); return(percent.days.op.threshold(ci@data$tmax, ci@dates, ci@jdays, ci@date.factors[[match.arg(freq)]], ci@quantiles$tmax$outbase$q90, ci@quantiles$tmax$inbase$q90, ci@base.range, ">", ci@max.missing.days[match.arg(freq)]) * ci@namasks[[match.arg(freq)]]$tmax) }
+
+################# END TEMPORARY INCLUSION FROM CLIMDEX.PCIC
+
+
+
+
 # tx50p
 # Percentage of days of days where TX>50th percentile
 # same as climdex.tx90p, except for 50th percentile
