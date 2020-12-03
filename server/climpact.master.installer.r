6d6bfa30d7525d70401002b251165bb7dc7d608a
heroldn <heroldn@mailbox.org>
2020-12-03 14:45:04 +1100

    Add RUnit to list of packages needed

--- server/climpact.master.installer.r
+++ server/climpact.master.installer.r
@@ -5,7 +5,7 @@
 
  packages <- c("abind","bitops","Rcpp","caTools","PCICt","SPEI","climdex.pcic","ncdf4","snow","udunits2","functional","proj4","foreach","doParallel","doSNOW","zoo","zyp","tcltk2",
  			"shiny","shinythemes","markdown","servr","dplyr","corrplot","ggplot2","shinyjs",
-			"shinydashboard","shinyBS","slickR","xml2","shinyWidgets","qpdf")
+			"shinydashboard","shinyBS","slickR","xml2","shinyWidgets","qpdf","Runit")
 
  # Print Unix-specific messages
  if(.Platform$OS.type == "unix") {
