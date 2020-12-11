7f9fe6a061f6b99c78048998f10cb2b4cc97d9a9
Jonathan McComb <jmccomb@infoasis.com.au>
2020-12-11 17:57:23 +1100

    Update test to only monitor necessary elements

--- tests/single_station_test.R
+++ tests/single_station_test.R
@@ -1,24 +1,31 @@
 app <- ShinyDriver$new("../", loadTimeout = 15000)
 app$snapshotInit("single_station_test", screenshot = FALSE)
 
-app$snapshot()
-app$snapshot()
+justthethings <- list(
+  input = c("process_single_station", "ui-baseEnd", "ui-baseStart",
+    "ui-stationName", "ui-stationLat", "ui-stationLon", "ui-dataFile"),
+  output = c("ui-indexCalculationErrors", "ui-indexCalculationStatus",
+    "ui-qcLink", "ui-qcStatus",
+    "ui-sectorCorrelationError", "ui-sectorCorrelationLink", "ui-sectorCorrelationStatus",
+    "ui-sectorDataFile", "ui-sectorPlotTitle", "ui-y_axis_label")
+)
+app$snapshot(item = justthethings)
 app$uploadFile(`ui-dataFile` = "../www/sample_data/sydney_observatory_hill_1936-2015.txt") # <-- This should be the path to the file, relative to the app's tests/ directory
 # app$waitForValue("ui-stationName", ignore = "")
-app$snapshot()
+app$snapshot(item = justthethings)
 app$setInputs(`ui-btn_next_step_1` = "click")
-app$setInputs(`ui-doQualityControl` = "click", values_ = FALSE)
-Sys.sleep(8)
-app$snapshot()
+app$setInputs(`ui-doQualityControl` = "click", wait_ = FALSE, values_ = FALSE)
+Sys.sleep(10)
+app$snapshot(item = justthethings)
 app$setInputs(`ui-btn_next_step_2` = "click")
-app$snapshot()
-app$setInputs(`ui-calculateIndices` = "click", values_ = FALSE)
-Sys.sleep(60)
-app$snapshot()
+app$snapshot(item = justthethings)
+app$setInputs(`ui-calculateIndices` = "click", wait_ = FALSE, values_ = FALSE)
+Sys.sleep(45)
+app$snapshot(item = justthethings)
 app$setInputs(`ui-btn_next_step_3` = "click")
-app$snapshot()
+app$snapshot(item = justthethings)
 app$uploadFile(`ui-sectorDataFile` = "../www/sample_data/wheat_yield_nsw_1922-1999.csv") # <-- This should be the path to the file, relative to the app's tests/ directory
-app$snapshot()
-app$setInputs(`ui-calculateSectorCorrelation` = "click", values_ = FALSE)
+app$snapshot(item = justthethings)
+app$setInputs(`ui-calculateSectorCorrelation` = "click", wait_ = FALSE, values_ = FALSE)
 Sys.sleep(30)
-app$snapshot()
+app$snapshot(item = justthethings)
