c1177789cbfd102a43107a82fa8f44c353460a76
heroldn <heroldn@mailbox.org>
2025-01-20 13:38:58 +1030

    make QC step repeatable

--- modules/singleStationStep2UI.R
+++ modules/singleStationStep2UI.R
@@ -16,12 +16,6 @@ singleStationStep2UI <- function (id) {
         condition = "output.loadDataError == ''",
         ns = ns,
         conditionalPanel(
-          condition = "output.qcStatus != 'Done'",
-          ns = ns,
-          div(style = "margin-top: 3em; display: block;"),
-          actionBttn(ns("doQualityControl"), label = " Check Data Quality", style = "jelly", color = "warning", icon = icon("play-circle", "fa-3x"))
-        ),
-        conditionalPanel(
           condition = "output.qcStatus == 'Done'",
           ns = ns,
           h4("2. Check data quality"),
@@ -33,15 +27,21 @@ singleStationStep2UI <- function (id) {
           ),
           htmlOutput(ns("qualityControlError")),
           slickROutput(ns("slickRQC"), width = "850px")
+        ),
+        conditionalPanel(
+          condition = "output.loadDataError == ''",
+          ns = ns,
+          div(style = "margin-top: 3em; display: block;"),
+          actionBttn(ns("doQualityControl"), label = " Check Data Quality", style = "jelly", color = "warning", icon = icon("play-circle", "fa-2x"))
         )
       ),
       conditionalPanel(
-        condition = "output.qcStatus != 'Done'",
+        condition = "output.loadDataError == ''",
         ns = ns,
         h4("Quality control parameters"),
         numericInput(ns("iqr_threshold_temp"), "Interquartile range (IQR) threshold for temperature outliers:", 3, min = 1, max = 10),
         bsTooltip(id = paste0(id, "-", "iqr_threshold_temp"), title = "The number of interquartile ranges used to identify temperature outliers", placement = "left", trigger = "hover"),
-        numericInput(ns("iqr_threshold_prec"), "Interquartile range (IQR) threshold for precipitation outliers:", 5, min = 1, max = 10),
+        numericInput(ns("iqr_threshold_prec"), "Interquartile range (IQR) threshold for precipitation outliers:", 5, min = 1, max = NA),
         bsTooltip(id = paste0(id, "-", "iqr_threshold_prec"), title = "The number of interquartile ranges used to identify precipitation outliers", placement = "left", trigger = "hover"),
         numericInput(ns("prec_threshold"), "Maximum daily rainfall threshold (mm):", 200, min = 0, max = 1500),
         bsTooltip(id = paste0(id, "-", "prec_threshold"), title = "Daily rainfall above this amount will be flagged for checking", placement = "left", trigger = "hover"),
